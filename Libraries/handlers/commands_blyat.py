68e8d3a6b400769c7e0c492351648c39b3af4c61
Q-Lukyss <quentin.wow@free.fr>
2024-09-29 14:38:41 +0200

    update error

--- Libraries/handlers/commands_blyat.py
+++ Libraries/handlers/commands_blyat.py
@@ -66,19 +66,19 @@ async def suce(update: Update, context) -> None:
     user_id = update.message.from_user.id
     message_suce_object = get_random_suce_messages
     if user_id == int(os.getenv("Lukyss_id")):
-        await update.message.reply_text(random(message_suce_object['moi']))
+        await update.message.reply_text(random.choice(message_suce_object['moi']))
         await context.bot.set_message_reaction(chat_id=chat_id, message_id=message_id, reaction='😈')
     elif user_id == int(os.getenv("Vincent_id")):
-        await update.message.reply_text(random(message_suce_object['vincent'] + message_suce_object["default"]))
+        await update.message.reply_text(random.choice(message_suce_object['vincent'] + message_suce_object["default"]))
         await context.bot.set_message_reaction(chat_id=chat_id, message_id=message_id, reaction=load_negative_emoji())
     elif user_id == int(os.getenv("Florian_id")):
-        await update.message.reply_text(random(message_suce_object['florian'] + message_suce_object["default"]))
+        await update.message.reply_text(random.choice(message_suce_object['florian'] + message_suce_object["default"]))
         await context.bot.set_message_reaction(chat_id=chat_id, message_id=message_id, reaction=load_negative_emoji())
     elif user_id == int(os.getenv("Guillaume_id")):
-        await update.message.reply_text(random(message_suce_object['guillaume'] + message_suce_object["default"]))
+        await update.message.reply_text(random.choice(message_suce_object['guillaume'] + message_suce_object["default"]))
         await context.bot.set_message_reaction(chat_id=chat_id, message_id=message_id, reaction=load_negative_emoji())
     else:
-        await update.message.reply_text(random(message_suce_object["default"]))
+        await update.message.reply_text(random.choice(message_suce_object["default"]))
         await context.bot.set_message_reaction(chat_id=chat_id, message_id=message_id, reaction=load_negative_emoji())
 
 
